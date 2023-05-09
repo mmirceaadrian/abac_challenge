46b0fa884364af42a9fe283b21d4301b50d5ef6e
mmirceaadrian <adrian.mircea9024@gmail.com>
2023-05-09 10:07:09 +0300

    add doc to session cubit

--- lib/src/bloc/session/session_cubit.dart
+++ lib/src/bloc/session/session_cubit.dart
@@ -2,13 +2,18 @@ import 'package:abac_challenge/src/bloc/session/session_state.dart';
 import 'package:abac_challenge/src/repository/auth_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// Cubit for the session
+///
+/// This cubit is used to manage the state of the session
 class SessionCubit extends Cubit<SessionState> {
+  /// Repository for the authentication
   final AuthRepo authRepo;
 
   SessionCubit({required this.authRepo}) : super(UnkownSessionState()) {
     attemptAutoLogin();
   }
 
+  /// This method is used to attempt the auto login
   void attemptAutoLogin() async {
     try {
       final token = await authRepo.attemptAutoLogin();
