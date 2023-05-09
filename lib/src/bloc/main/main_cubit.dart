79e6d19c5ae5fd1b3a44dd0648d831b5924d1eef
mmirceaadrian <adrian.mircea9024@gmail.com>
2023-05-09 10:05:39 +0300

    add documentation to main cubit

--- lib/src/bloc/main/main_cubit.dart
+++ lib/src/bloc/main/main_cubit.dart
@@ -1,12 +1,19 @@
 import 'package:abac_challenge/src/bloc/main/main_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// Cubit for the main screen
+///
+/// This cubit is used to manage the state of the main screen
 class MainCubit extends Cubit<MainState> {
   MainCubit() : super(SpaceshipsState());
 
   void showSpaceships() => emit(SpaceshipsState());
   void showAppointments() => emit(AppointmentsState());
 
+  /// This method is used to show the screen by index
+  ///
+  /// 0 -> Spaceships
+  /// 1 -> Appointments
   void showByIndex(index) {
     switch (index) {
       case 0:
